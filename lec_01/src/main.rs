fbb6cea76521103983d4fbd4bb60f255714f99bc
rabbritva <rabbritva@gmail.com>
2026-09-04 20:28:39 +0500

    pr_01

---
+++ lec_01/src/main.rs
@@ -0,0 +1,51 @@
+use std::io::{self, Write};
+
+fn main() {
+    
+    // first(); //01
+    // second() //02
+    // third(); //03
+}
+
+const MSG_DIGIT: &str = "Введите целое число";
+const MSG_IN: &str = "Failed to read line";
+
+fn first() -> (){
+    let mut name: String = String::new();
+    print!("Введите ваше имя: ");
+    io::stdout().flush().expect("!");
+    io::stdin().read_line(&mut name).expect("Failed to read line");
+    println!("Hello, {}!", name.trim());
+}
+
+fn second() -> (){
+    let mut input: String = String::new();
+    print!("Введите два числа: \na = ");
+    io::stdout().flush().expect("!");
+    let msg: String = "Введите целое число".to_string();
+    io::stdin().read_line(&mut input).expect("Failed read line");
+    let a: i32 = input.trim().parse().expect(&msg);
+    input.clear();
+    print!("b = ");
+    io::stdout().flush().expect("!");
+    io::stdin().read_line(&mut input).expect("Failed read line");
+    let b: i32 = input.trim().parse().expect(&msg);
+    input.clear();
+    println!("Сумма введенных чисел: {}", a+b);
+}
+
+fn third() -> (){
+    let mut input: String = String::new();
+    println!("Введите два числа:");
+    print!("a = ");
+    io::stdout().flush().expect("Flash error");
+    io::stdin().read_line(&mut input).expect(&MSG_IN);
+    let a: i32 = input.trim().parse().expect(&MSG_DIGIT);
+    input.clear();
+    print!("a = ");
+    io::stdout().flush().expect("Flash error");
+    io::stdin().read_line(&mut input).expect(&MSG_IN);
+    let b: i32 = input.trim().parse().expect(&MSG_DIGIT);
+    input.clear();
+    println!("a - b = {}", a-b);
+}
